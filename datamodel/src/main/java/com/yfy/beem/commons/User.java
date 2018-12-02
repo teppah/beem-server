36e968a60cc304215865fd118dadd08141f1f685
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-01 23:55:46 -0500

    updated pom.xml and datamodel classes

---
+++ datamodel/src/main/java/com/yfy/beem/commons/User.java
@@ -0,0 +1,30 @@
+package com.yfy.beem.commons;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.net.InetAddress;
+import java.security.PublicKey;
+
+/**
+ * The base user representation for the server module of the Beem chat client.
+ * TODO: Write javadoc for server:com.yfy.beem.commons.User
+ * */
+@Entity(name ="user")
+@Data
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final long id;
+    private String name;
+    private final InetAddress ipAddress;
+    private final PublicKey publicKey;
+
+}
---
+++ datamodel/src/test/java/UserTest.java
@@ -0,0 +1,13 @@
+class UserTest {
+//    @Test
+//    void ipAddressTest()  {
+//        com.yfy.beem.commons.User user = null;
+//        try {
+//            user = new com.yfy.beem.commons.User(0, "Test name", InetAddress.getLocalHost(), BouncyGPG);
+//            Assert.assertEquals("Ip addresses not equal", user.getIpAddress(),InetAddress.getByName("127.0.0.1"));
+//        } catch (IOException e){
+//            Assert.fail(e.getMessage());
+//        }
+//    }
+//    TODO: write unit test for com.yfy.beem.commons.User
+}
