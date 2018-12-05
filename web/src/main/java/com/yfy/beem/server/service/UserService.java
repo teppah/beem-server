5b76078d8a54a86ce2b51b28e01b3ed888fa25bd
Unknown <yifengyang01@gmail.com>
2018-12-05 08:39:29 -0500

    added preliminary javadoc

--- web/src/main/java/com/yfy/beem/server/service/UserService.java
+++ web/src/main/java/com/yfy/beem/server/service/UserService.java
@@ -8,10 +8,12 @@ import java.util.List;
 
 /**
  * Abstraction layer for accessing the database
+ * // TODO: write javadoc for each method
  * */
 public interface UserService {
     List<User> getActiveUsers();
     boolean addUser(User user);
+    boolean deleteUserByUUID(long id);
     User retrieveUserByUUID(long id);
     List<User> retrieveUsersMatchingName(String name);
 
