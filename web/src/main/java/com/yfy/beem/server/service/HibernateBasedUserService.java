2ab4656b6c5212dc27aae34f2f6353210aa2ed73
Unknown <yifengyang01@gmail.com>
2018-12-05 09:49:13 -0500

    removed unnecessary imports

--- web/src/main/java/com/yfy/beem/server/service/HibernateBasedUserService.java
+++ web/src/main/java/com/yfy/beem/server/service/HibernateBasedUserService.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
