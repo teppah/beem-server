09141ff623c41f09cb41bf314babea2da9a98839
Unknown <yifengyang01@gmail.com>
2018-12-05 14:48:46 -0500

    implemented addUser REST API endpoint functionnality
    preliminary implementation of the mechanism for saving the user to Hibernate database.
    still need to verify whether a change in the User class is needed.
    still need to verify if get remote ip address works or not.

--- web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
+++ web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
@@ -1,11 +1,25 @@
 package com.yfy.beem.server.controller;
 
+import com.yfy.beem.server.datamodel.User;
 import com.yfy.beem.server.respository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOError;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @RestController
 @Slf4j
@@ -21,5 +35,34 @@ public class RestApiController {
     public String test() {
         log.info("userRepository = {}", userRepository);
         return "test";
+    }
+
+    @PostMapping("registerUser")
+    public String registerUser (@RequestParam long id,
+                                @RequestParam String name,
+                                @RequestParam String publicKey,
+                                HttpServletRequest request) {
+        // remove all superfluous spaces, beginning tag and end tag of RSA public key
+        publicKey.replaceAll("\\n","").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
+
+        try {
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+            RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+            User user = new User(id,name, InetAddress.getByName(request.getRemoteAddr()), pubKey);
+            userRepository.save(user);
+            log.info("Successfully saved new user: {}", user);
+            return user.toString();
+        } catch (NoSuchAlgorithmException e) {
+            log.error("exception related to KeyFactory: {}", e.getMessage());
+            return "ERROR";
+        } catch (InvalidKeySpecException e) {
+            log.error("exception related to public key generation: {}", e.getMessage());
+            return "ERROR";
+        } catch (IOException e) {
+            log.error("IOException: {}", e.getMessage());
+            return "ERROR";
+        }
+
     }
 }
