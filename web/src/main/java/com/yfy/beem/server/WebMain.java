8ab9808b3dcd3d5b11aa882b51b83d45cf7f8d1f
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-05 19:24:36 -0500

    removed unnecessary @ComponentScan in Main class

--- web/src/main/java/com/yfy/beem/server/WebMain.java
+++ web/src/main/java/com/yfy/beem/server/WebMain.java
@@ -3,10 +3,8 @@ package com.yfy.beem.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.yfy.beem")
 public class WebMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(WebMain.class);
