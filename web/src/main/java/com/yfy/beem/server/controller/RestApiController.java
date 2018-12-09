79a54c182e16e3bb379d672aad45c21b3a626d9c
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 19:45:24 -0500

    added handler method for GET_USERS endpoint

--- web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
+++ web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
@@ -5,6 +5,7 @@ import com.yfy.beem.server.respository.UserRepository;
 import com.yfy.beem.server.util.RequestMappings;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,9 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @Slf4j
@@ -29,8 +33,8 @@ public class RestApiController {
     public RestApiController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
-    @PostMapping(RequestMappings.POST_TEST)
+
+    @PostMapping(RequestMappings.REGISTER_USER)
     public String registerUser (@RequestParam long id,
                                 @RequestParam String name,
                                 @RequestParam String publicKey,
@@ -59,6 +63,15 @@ public class RestApiController {
             log.error("IOException: {}", e.getMessage());
             return "ERROR";
         }
+    }
 
+    @GetMapping(RequestMappings.GET_USERS)
+    public User[] getActiveUsers() {
+        List<User> users = StreamSupport
+                .stream(userRepository.findAll().spliterator(),false)
+                .collect(Collectors.toList());
+        User[] userArray = new User[users.size()];
+        userArray = users.toArray(userArray);
+        return userArray;
     }
 }
