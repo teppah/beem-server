23837ab42b18e83ba0b6ae38a6f146c52f8388aa
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 20:23:20 -0500

    added ADD_USER viewname constant

---
+++ web/src/main/java/com/yfy/beem/server/util/ViewNames.java
@@ -0,0 +1,12 @@
+package com.yfy.beem.server.util;
+
+/**
+ * Names of view templates to display to user
+ * */
+public class ViewNames {
+    // do not let anyone instantiate
+    private ViewNames() {}
+
+    // ------ CONSTANTS ------
+    public static final String ADD_USER = "add_user";
+}
