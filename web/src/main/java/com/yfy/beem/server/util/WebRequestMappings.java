fb83bc6cf8fc7597febbaafcfc3ac4b1c61e5fe7
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 20:23:50 -0500

    added new mapping constants class for web requests

---
+++ web/src/main/java/com/yfy/beem/server/util/WebRequestMappings.java
@@ -0,0 +1,12 @@
+package com.yfy.beem.server.util;
+
+/**
+ * Mappings for web pages
+ * */
+public class WebRequestMappings {
+    // do not let anyone instantiate this
+    private WebRequestMappings() {}
+
+    // ------ CONSTANTS -----
+    public static final String ADD_USER = "adduser";
+}
