be5c542088e2e7acf16ff1f14fa71fa90fade021
Unknown <yifengyang01@gmail.com>
2018-12-08 19:15:50 -0500

    created central request mapping constants class

---
+++ web/src/main/java/com/yfy/beem/server/util/RequestMappings.java
@@ -0,0 +1,13 @@
+package com.yfy.beem.server.util;
+
+
+/**
+ * Centralized request mapping constants
+ * */
+public class RequestMappings {
+    //private constructor, do not let anybody instantiate this class
+    private RequestMappings() {}
+
+    // ----- MAPPINGS ------
+    public static final String POST_TEST = "registerUser";
+}
