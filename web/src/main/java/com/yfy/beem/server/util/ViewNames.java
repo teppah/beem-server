226a598d81b4a0e5fb0c0d7ba5476a67944f68ef
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 21:52:31 -0500

    added view names for USER and REDIRECT USER

--- web/src/main/java/com/yfy/beem/server/util/ViewNames.java
+++ web/src/main/java/com/yfy/beem/server/util/ViewNames.java
@@ -9,4 +9,6 @@ public class ViewNames {
 
     // ------ CONSTANTS ------
     public static final String ADD_USER = "add_user";
+    public static final String LIST_USERS = "users";
+    public static final String REDIRECT_LIST_USERS = "redirect:/" + LIST_USERS;
 }
