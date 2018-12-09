160d5aeafa2fc13893602e4f3344808edbb58aba
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 19:55:01 -0500

    removed key-checking logic from controller
    this will be implemented at a later date in a service; will be optional

--- web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
+++ web/src/main/java/com/yfy/beem/server/controller/RestApiController.java
@@ -11,15 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.nio.charset.Charset;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -35,40 +26,24 @@ public class RestApiController {
     }
 
     @PostMapping(RequestMappings.REGISTER_USER)
-    public String registerUser (@RequestParam long id,
-                                @RequestParam String name,
-                                @RequestParam String publicKey,
-                                HttpServletRequest request) {
+    public String registerUser(@RequestParam long id,
+                               @RequestParam String name,
+                               @RequestParam String publicKey,
+                               HttpServletRequest request) {
         // remove all superfluous spaces, beginning tag and end tag of RSA public key
-        publicKey = publicKey.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replaceAll(" ","");
+//        publicKey = publicKey.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replaceAll(" ","");
+        log.info("httpServletRequest = {}", request);
         log.info("publicKey = {}", publicKey);
-
-        try {
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64
-                    .getDecoder()
-                    .decode(publicKey.getBytes(Charset.defaultCharset())));
-            RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
-            User user = new User(id,name, InetAddress.getByName(request.getRemoteAddr()), pubKey);
-            userRepository.save(user);
-            log.info("Successfully saved new user: {}", user);
-            return user.toString();
-        } catch (NoSuchAlgorithmException e) {
-            log.error("exception related to KeyFactory: {}", e.getMessage());
-            return "ERROR";
-        } catch (InvalidKeySpecException e) {
-            log.error("exception related to public key generation: {}", e.getMessage());
-            return "ERROR";
-        } catch (IOException e) {
-            log.error("IOException: {}", e.getMessage());
-            return "ERROR";
-        }
+        User user = new User(id, name, request.getRemoteAddr(), publicKey);
+        userRepository.save(user);
+        log.info("Successfully saved new user: {}", user);
+        return user.toString();
     }
 
     @GetMapping(RequestMappings.GET_USERS)
     public User[] getActiveUsers() {
         List<User> users = StreamSupport
-                .stream(userRepository.findAll().spliterator(),false)
+                .stream(userRepository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
         User[] userArray = new User[users.size()];
         userArray = users.toArray(userArray);
