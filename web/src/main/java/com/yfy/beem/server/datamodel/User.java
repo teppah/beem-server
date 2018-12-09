cb7399ebbc2cc8977451ad9c67bc3f18269eccda
Unknown <yifengyang01@gmail.com>
2018-12-09 10:19:34 -0500

    removed @GeneratedValue for entity to specify own long id
    will probably change this later to, instead of using a long as UID, i'll specify a string UUID instead, this will probably be better for custom ID generation and it will be more specific

--- web/src/main/java/com/yfy/beem/server/datamodel/User.java
+++ web/src/main/java/com/yfy/beem/server/datamodel/User.java
@@ -19,7 +19,7 @@ import javax.persistence.*;
 @ToString
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String ipAddress;
