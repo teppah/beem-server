41c77801e7c6b4bab3cdfd5ffff1493494e46f52
Yi Feng Yang <yifengyang01@gmail.com>
2018-12-08 22:15:06 -0500

    refactor api endpoint name

--- web/src/main/java/com/yfy/beem/server/util/ApiMappings.java
+++ web/src/main/java/com/yfy/beem/server/util/ApiMappings.java
@@ -5,10 +5,10 @@ package com.yfy.beem.server.util;
  * Centralized request mapping constants
  * */
 public class ApiMappings {
-    //private constructor, do not let anybody instantiate this class
+    // do not let anybody instantiate this class
     private ApiMappings() {}
 
     // ----- MAPPINGS ------
-    public static final String REGISTER_USER = "registerUser";
+    public static final String REGISTER_USER = "register-user";
     public static final String GET_USERS = "active";
 }
